2b725ede691a456e2516295779336eab98ccb131
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-07-26 00:26:01 +0200

    fix: alias issue

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    esmExternals: 'loose',
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': './components',
+      '@lib': './lib',
+    };
+    return config;
   },
 };
 
